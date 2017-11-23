faebe86d87c502d387d6c924742ea5333ec04596
abennsir <abennsir.externe@pagesjaunes.fr>
2017-11-23 17:15:55 +0100

    Structure project

---
+++ app/src/main/java/com/hatem/noureddine/carrefour/demo/presenter/VideoDetailPresenter.java
@@ -0,0 +1,14 @@
+package com.hatem.noureddine.carrefour.demo.presenter;
+
+/**
+ *
+ *
+ * created on 23/11/2017
+ *
+ * @author anisbennsir
+ * @version
+ */
+public interface VideoDetailPresenter {
+
+
+}
